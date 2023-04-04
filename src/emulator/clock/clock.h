661642429dc53209fcb026f544d826b40fade5c4
Daniel Shubert <dkshubert@gmail.com>
2023-04-03 23:57:30 -0700

    Fix formatting

--- src/emulator/clock/clock.h
+++ src/emulator/clock/clock.h
@@ -9,26 +9,23 @@ namespace unnes {
 
 class Logger;
 
-/// @brief An interface for any device connected to the clock (like the CPU or
-/// PPU). When a clock tick occurs, any device implementing this interface that
-/// has connected itself to the Clock via Clock::connect will be notified of the
-/// clock tick via its handClockTick implementation.
+/// @brief An interface for any device connected to the clock (like the CPU or PPU). When a clock
+/// tick occurs, any device implementing this interface that has connected itself to the Clock via
+/// Clock::connect will be notified of the clock tick via its handClockTick implementation.
 ///
 /// @see Clock::connect
 struct IClockedDevice {
     virtual ~IClockedDevice() = default;
 
-    /// @brief Hardware components react to each cock tick via their
-    /// implementation of this method.
+    /// @brief Hardware components react to each cock tick via their implementation of this method.
     ///
     /// @param tickNum The exact clock tick number since emulator boot.
     virtual void handleClockTick(std::uint64_t tickNum) = 0;
 };
 
 class Clock {
-    /// @note _speedHz isn't const because we want to be able to adjust the
-    /// speed of emulation dynamically, based on the user's application
-    /// settings.
+    /// @note _speedHz isn't const because we want to be able to adjust the speed of emulation
+    /// dynamically, based on the user's application settings.
     std::uint32_t _speedHz{ntsc::kSpeedHz};
     std::uint64_t _totalTicks{0};
     std::vector<IClockedDevice*> _connectedDevices{};
@@ -37,9 +34,8 @@ class Clock {
    public:
     Clock(Logger& logger);
 
-    /// @brief Connects a set of devices to this clock. These devices will be
-    /// notified on each clock tick, via the IClockedDevice::handleClockTick
-    /// function.
+    /// @brief Connects a set of devices to this clock. These devices will be notified on each clock
+    /// tick, via the IClockedDevice::handleClockTick function.
     ///
     /// @param devices The devices to connect to this clock instance.
     void connect(const std::vector<IClockedDevice*>& devices);
