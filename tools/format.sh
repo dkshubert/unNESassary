4cc4c799d9550128bc4ea03a6586acdede78b4b6
Daniel Shubert <dkshubert@gmail.com>
2023-04-03 23:57:11 -0700

    Create dedicated formatting script

--- tools/format.sh
+++ tools/format.sh
@@ -4,6 +4,5 @@ SCRIPT_DIR=$(dirname $0)
 PROJECT_ROOT=$SCRIPT_DIR/..
 
 cd $PROJECT_ROOT
-./tools/format.sh
-./tools/build.sh
-./tools/run.sh
+find . -iname *.h -o -iname *.cpp | xargs clang-format -i
+
