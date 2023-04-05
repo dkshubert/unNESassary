e475fe12e21ecde73ecd1b19f47074380a7bbcc5
Daniel Shubert <dkshubert@gmail.com>
2023-04-04 22:24:29 -0700

    Update compiler warnings

--- tools/build.sh
+++ tools/build.sh
@@ -8,8 +8,5 @@ cd $PROJECT_ROOT
 conan install . --output-folder=$BUILD_DIR # --build missing
 
 cd $BUILD_DIR
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake \
--DCMAKE_BUILD_TYPE=Release \
--DCOMPILE_WARNING_AS_ERROR=True
-
+cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build .
