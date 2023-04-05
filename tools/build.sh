54f8a69dbd45862f9391fb40cf3612c3a83b7d97
Daniel Shubert <dkshubert@gmail.com>
2023-04-04 23:02:57 -0700

    Update build helper script

--- tools/build.sh
+++ tools/build.sh
@@ -5,8 +5,8 @@ PROJECT_ROOT=$SCRIPT_DIR/..
 BUILD_DIR=$PROJECT_ROOT/build
 
 cd $PROJECT_ROOT
-conan install . --output-folder=$BUILD_DIR # --build missing
+conan install . --output-folder=$BUILD_DIR
 
 cd $BUILD_DIR
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
