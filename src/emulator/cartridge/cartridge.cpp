87d351a49bd49b51a8756078bba694e15e0ad08e
Daniel Shubert <dkshubert@gmail.com>
2023-04-05 21:07:02 -0700

    Remove cruft from Cartridge

--- src/emulator/cartridge/cartridge.cpp
+++ src/emulator/cartridge/cartridge.cpp
@@ -20,11 +20,6 @@ bool Cartridge::load(const std::string_view romPath)
 
     std::ifstream rom { _romPath, std::ios_base::binary | std::ios::in };
 
-    // if (!rom) {
-    //     _logger.write(LogLevel::warn, fmt::format("Failed to load ROM"));
-    //     return false;
-    // }
-
     // TODO : parse ROM header and load memory buffers + provide accessors to cart memory
 
     _logger.write(LogLevel::info, fmt::format("Successfully loaded ROM"));
